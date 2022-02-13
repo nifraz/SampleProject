98f3ee1596b668aaa5c07c3f95dc241fe0dce687
Nifraz Navahz <nifraz@live.com>
2022-02-13 16:00:42 +0530

    Created GetPow method

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace sampleproject
             var name = Console.ReadLine();
             Console.WriteLine($"Hello {name}. Hi there, and Welcome...");
             Console.WriteLine("This is Nifraz!");
+            Console.WriteLine($"5 to the power of 3 is: {GetPow(5, 3)}");
+        }
+
+        static double GetPow(int b, int e){
+            return Math.Pow(b, e);
         }
     }
 }
